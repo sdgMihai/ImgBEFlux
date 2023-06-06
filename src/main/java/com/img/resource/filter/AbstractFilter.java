2926b922de37ab6fae1baf0bfa94caa6cb3710a9
Mihai Gheoace <mihai.gheoace@allcloud.io>
2023-06-06 21:46:44 +0300

    replace c. future with mono in int+abstract

--- src/main/java/com/img/resource/filter/AbstractFilter.java
+++ src/main/java/com/img/resource/filter/AbstractFilter.java
@@ -4,10 +4,11 @@ import com.img.resource.utils.Image;
 import com.img.resource.utils.ImageUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.util.Pair;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
-import java.util.stream.Stream;
 
 @Slf4j
 public class AbstractFilter implements Filter{
@@ -16,24 +17,16 @@ public class AbstractFilter implements Filter{
      * @param in          input image reference.
      * @param out         output image reference.
      * @param PARALLELISM integer value denoting the number of task running in parallel.
+     * @return Mono<Image> The processed Image
      */
-    public void applyFilter(Image in, Image out, final int PARALLELISM, final Executor executor) {
-        CompletableFuture<Void>[] partialFilters = new CompletableFuture[PARALLELISM];
+    public Mono<Image> applyFilter(Image in, Image out, final int PARALLELISM, final Executor executor) {
         Pair<Integer, Integer>[] ranges = ImageUtils.getRange(PARALLELISM, in.height);
-        for (int i = 0; i < PARALLELISM; i++) {
-            int start = ranges[i].getFirst();
-            int stop = ranges[i].getSecond();
-            partialFilters[i] = CompletableFuture.runAsync(
-                    () -> applyFilterPh1(in, out, start, stop)
-                    , executor
-            );
-        }
-//        CompletableFuture.allOf(partialFilters).join();
-        Stream.of(partialFilters)
-                .map(CompletableFuture::join)
-                .forEach((Void) -> {
-                    log.debug("finish abstract");
-                });
+        return Flux.fromArray(ranges)
+                .flatMap(range -> Mono.fromRunnable(() ->
+                                applyFilterPh1(in, out, range.getFirst(), range.getSecond())
+                        ).subscribeOn(Schedulers.fromExecutor(executor))
+                )
+                .then(Mono.just(out));
     }
 
     /**
--- src/main/java/com/img/resource/filter/Filter.java
+++ src/main/java/com/img/resource/filter/Filter.java
@@ -1,9 +1,10 @@
 package com.img.resource.filter;
 
 import com.img.resource.utils.Image;
+import reactor.core.publisher.Mono;
 
 import java.util.concurrent.Executor;
 
 public interface Filter {
-    void applyFilter(Image in, Image out, final int PARALLELISM, final Executor executor);
+    Mono<Image> applyFilter(Image in, Image out, final int PARALLELISM, final Executor executor);
 }
